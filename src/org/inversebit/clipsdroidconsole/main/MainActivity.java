e2a727aa4e4f52b1098bdfbfce887bf521bef2ab
Inversebit <inversebit@gmail.com>
2014-02-26 18:14:34 +0100

    Welcome message [Added] Welcome message to app in MainActivity

--- src/org/inversebit/clipsdroidconsole/main/MainActivity.java
+++ src/org/inversebit/clipsdroidconsole/main/MainActivity.java
@@ -1,9 +1,11 @@
 package org.inversebit.clipsdroidconsole.main;
 
 
+import eu.deustotech.clips.Environment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity
@@ -14,12 +16,16 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		((TextView)findViewById(R.id.tVWelcome))
+			.setText(
+				getString(R.string.welcomeMessage) + " " +
+				Environment.getCLIPSVersion());
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
